045938acee68c5488bcc31bbecd66082c0893453
Igor <kalamejcevigor@gmail.com>
2024-10-04 22:03:11 +0300

    Miner

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-async function sendRequest(url, method, data) {
-    url = `https://tg-api.tehnikum.school/tehnikum_course/minesweeper/${url}`
 
-    if (method == "POST") {
+
+// Отправка запросов
+async function sendRequest(url, method, data) {
+    url = `https://tg-api.tehnikum.school/tehnikum_course/minesweeper/${url}`;
+
+    if (method === "POST") {
         let response = await fetch(url, {
             method: "POST",
             headers: {
@@ -10,10 +13,9 @@ async function sendRequest(url, method, data) {
             },
             body: JSON.stringify(data)
         });
-
         response = await response.json();
         return response;
-    } else if (method == "GET") {
+    } else if (method === "GET") {
         url = url + "?" + new URLSearchParams(data);
         let response = await fetch(url, {
             method: "GET",
@@ -22,95 +24,85 @@ async function sendRequest(url, method, data) {
                 'Content-Type': 'application/json'
             }
         });
-
         response = await response.json();
         return response;
     }
 }
 
-let username 
-let balance
-let points = 1000
-checkUser()
+let username;
+let balance;
+let points = 1000;
+let game_id;
 
-let autorizationForm = document.getElementById("autorization")
-autorizationForm.addEventListener("submit", autorization)
+checkUser();
 
-async function autorization() {
-    event.preventDefault()
+let autorizationForm = document.getElementById("autorization");
+autorizationForm.addEventListener("submit", autorization);
+
+async function autorization(event) {
+    event.preventDefault();
 
     const formData = new FormData(event.target);
     username = formData.get('username');
-    
 
-    let response = await sendRequest("user", "GET", {username})
-    if (response.error){
-        // пользоватьель не нацден, значит надоо его регестрировать
-        let regResponse = await sendRequest ("user", "POST", {username})
-        if(regResponse.error){
-            // Возникла ошибка при регистрации 
-            alert(regResponse.message)
+    let response = await sendRequest("user", "GET", { username });
+    if (response.error) {
+        // Пользователь не найден, регистрируем
+        let regResponse = await sendRequest("user", "POST", { username });
+        if (regResponse.error) {
+            alert(regResponse.message);
         } else {
-            // успешная регистрация 
-            balance = regResponse.balance
-            showUser()
+            balance = regResponse.balance;
+            showUser();
         }
     } else {
-        //Пользователь найден
-        balance = response.balance
-        showUser()
+        // Пользователь найден
+        balance = response.balance;
+        showUser();
     }
-
-   
 }
 
-function showUser(){
-    let popUpSection = document.querySelector('section')
-    popUpSection.style.display = "none"
-    let userInfo = document.querySelector("header span")
-    userInfo.innerHTML = `[${username}, ${balance}]`
+function showUser() {
+    let popUpSection = document.querySelector('section');
+    popUpSection.style.display = "none";
+    let userInfo = document.querySelector("header span");
+    userInfo.innerHTML = `[${username}, ${balance}]`;
 
-    localStorage.setItem("username", username)
+    localStorage.setItem("username", username);
 
-    if(localStorage.getItem("game_id")){
-        gameButton.setAttribute("data-game", "stop")
+    if (localStorage.getItem("game_id")) {
+        gameButton.setAttribute("data-game", "stop");
     } else {
-        gameButton.setAttribute("data-game", "start")
+        gameButton.setAttribute("data-game", "start");
     }
 }
 
-document.querySelector(".exit").addEventListener("click", exit)
+document.querySelector(".exit").addEventListener("click", exit);
 
-function exit(){
-    let popUpSection = document.querySelector('section')
-    popUpSection.style.display = "flex"
-    let userInfo = document.querySelector("header span")
-    userInfo.innerHTML = `[]`
+function exit() {
+    let popUpSection = document.querySelector('section');
+    popUpSection.style.display = "flex";
+    let userInfo = document.querySelector("header span");
+    userInfo.innerHTML = `[]`;
 
-    localStorage.removeItem("username")
+    localStorage.removeItem("username");
 }
 
-async function checkUser(){
-if(localStorage.getItem("username")){
-    username = localStorage.getItem("username")
-    let response = await sendRequest("user", "GET", {username})
-    if (response.error){
-        alert(response.message)
+async function checkUser() {
+    if (localStorage.getItem("username")) {
+        username = localStorage.getItem("username");
+        let response = await sendRequest("user", "GET", { username });
+        if (response.error) {
+            alert(response.message);
+        } else {
+            balance = response.balance;
+            showUser();
+        }
     } else {
-        balance = response.balance
-        showUser()
+        let popUpSection = document.querySelector('section');
+        popUpSection.style.display = "flex";
     }
-    
-} else {
-    let popUpSection = document.querySelector('section')
-    popUpSection.style.display = "flex"
 }
-}
-
-
-
-
-
 
 let pointBtns = document.getElementsByName("point");
 pointBtns.forEach((elem) => {
@@ -119,7 +111,7 @@ pointBtns.forEach((elem) => {
 
 function setPoints() {
     let checkedBtn = document.querySelector("input:checked");
-    if (checkedBtn) { // Проверка на наличие выбранной кнопки
+    if (checkedBtn) {
         points = +checkedBtn.value;
         console.log(points);
     } else {
@@ -132,14 +124,98 @@ gameButton.addEventListener("click", startOrStopGame);
 
 function startOrStopGame() {
     let option = gameButton.getAttribute("data-game");
-    if (option == "start") {
-        if (points >= 0) { // Исправлено условие
+    if (option === "start") {
+        if (points >= 0) {
             startGame();
         } else {
             alert("Points must be 0 or greater to start the game.");
         }
-    } else if (option == "stop") {
-        stopGame(); // Добавьте функцию stopGame для логики остановки игры
+    } else if (option === "stop") {
+        stopGame();
+    }
+}
+
+function activateArea() {
+    let cell = document.querySelectorAll(".cell");
+    let columns = 10; 
+    let rows = 8
+    cell.forEach((cell, i) => {
+        setTimeout(() => {
+            let row = Math.floor(i / columns);
+            let column = i - row*columns;
+
+            cell.setAttribute('data-row', row);
+            cell.setAttribute('data-column', column);
+            cell.classList.add("active");
+            cell.addEventListener("contextmenu", setFlag);
+            cell.addEventListener("click", makeStep);
+        }, 30 * i);
+    });
+}
+
+function setFlag() {
+    event.preventDefault();
+    let cell = event.target;
+    cell.classList.toggle("flag");
+}
+
+async function makeStep() {
+    let cell = event.target
+    let row = +cell.getAttribute("data-row");
+    let column = +cell.getAttribute("data-column");
+
+    let response = await sendRequest("game_step", "POST", { game_id, row, column });
+    if (response.error) {
+        alert(response.message);
+    } else {
+        if (response.status == "Won") {
+            updateArea(response.table)
+
+            balance = response.balance;
+            showUser();
+            clearArea();
+            gameButton.setAttribute("data-game", "start");
+            gameButton.innerHTML = "Играть";
+            alert("Ты выиграл!")
+
+        } else if (response.status == "Failed") {
+            updateArea(response.table)
+
+            balance = response.balance;
+            showUser();
+            clearArea();
+            gameButton.setAttribute("data-game", "start");
+            gameButton.innerHTML = "Играть";
+            alert("Ты проиграл:(")
+        } else if (response.status == "Ok") {
+            updateArea(response.table);
+
+        }
+    }
+
+    console.log(cell, row, column);
+}
+
+function updateArea(table) {
+    let cells = document.querySelectorAll(".cell");
+    let j = 0
+    for(let row = 0; row < table.length; row++){
+        for (let column = 0; column < table[row].length; column++){
+            let value = table[row][column]
+            if(value === 0) {
+                cells[j].classList.remove("active")
+                cells[j].classList.remove("flag")
+            } else if (value >= 1) {
+                cells[j].classList.remove("active")
+                cells[j].classList.remove("flag")
+                cells[j].innerHTML = value
+            } else if (value == "BOMB") {
+                cells[j].classList.remove("active")
+                cells[j].classList.remove("flag")
+                cells[j].classList.add("bomb")
+            }
+            j++
+        }
     }
 }
 
@@ -148,11 +224,32 @@ async function startGame() {
     if (response.error) {
         alert(response.message);
     } else {
-        console.log(response);
+        game_id = response.game_id;
+        gameButton.setAttribute("data-game", "stop");
+        gameButton.innerHTML = "Завершить игру";
+        clearArea();
+        activateArea();
     }
 }
 
-function stopGame() {
-    // Логика остановки игры
-    console.log("Game stopped");
+async function stopGame() {
+    let response = await sendRequest("stop_game", "POST", { username, game_id });
+    if (response.error) {
+        alert(response.message);
+    } else {
+        balance = response.balance;
+        showUser();
+        game_id = "";
+        gameButton.setAttribute("data-game", "start");
+        gameButton.innerHTML = "Играть";
+        clearArea();
+    }
+}
+
+function clearArea() {
+    let area = document.querySelector(".area");
+    area.innerHTML = "";
+    for (let i = 0; i < 80; i++) {
+        area.innerHTML += `<div class="cell"></div>`;
+    }
 }
